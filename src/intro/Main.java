c3f2df438e11debd60d74fd78ba77c6590ea2ee8
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-04-21 22:18:33 +0300

    java kamp

---
+++ src/intro/Main.java
@@ -0,0 +1,15 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		//Don't repeat yourself
+		String internetSubeButonu = "Internet Subesi";
+		
+		System.out.println(internetSubeButonu);
+		
+	}
+
+}
